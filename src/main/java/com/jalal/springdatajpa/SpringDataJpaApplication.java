af30f01976173516394189be4d32396c80683977
capablanca2 <jalal.abdelilah2@gmail.com>
2023-10-12 21:30:05 +0100

    adding student repository and testing save

--- src/main/java/com/jalal/springdatajpa/SpringDataJpaApplication.java
+++ src/main/java/com/jalal/springdatajpa/SpringDataJpaApplication.java
@@ -1,7 +1,9 @@
 package com.jalal.springdatajpa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringDataJpaApplication {
@@ -9,5 +11,11 @@ public class SpringDataJpaApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringDataJpaApplication.class, args);
     }
-
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+        return args -> {
+            Student jalal = new Student("Abde","jalal","a.jalal@gmail.com",23);
+            studentRepository.save(jalal);
+        };
+    }
 }
--- src/main/java/com/jalal/springdatajpa/Student.java
+++ src/main/java/com/jalal/springdatajpa/Student.java
@@ -8,10 +8,16 @@ import lombok.ToString;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @ToString
 @Entity(name = "student")
 public class Student {
+    public Student(String firstName, String lastName, String email, Integer age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
+
     @Id
     @SequenceGenerator(
             name = "student_sequence",
