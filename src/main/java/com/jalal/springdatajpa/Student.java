7bf4297cf2420722f0a0968d00c7af751f11d684
capablanca2 <jalal.abdelilah2@gmail.com>
2023-10-12 21:16:53 +0100

    adding student entity

---
+++ src/main/java/com/jalal/springdatajpa/Student.java
@@ -0,0 +1,57 @@
+package com.jalal.springdatajpa;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity(name = "student")
+public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
+    private long id;
+
+    @Column(
+            name = "first_name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String firstName;
+
+    @Column(
+            name = "last_name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String lastName;
+
+    @Column(
+            name = "email",
+            nullable = false,
+            unique = true
+    )
+    private String email;
+
+    @Column(
+            name = "age",
+            nullable = false
+    )
+    private Integer age;
+}
